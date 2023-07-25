7272c2a2c4b338bc46024a9c3be0b480a44d23cb
HimashaNethmini <himashanethmini05@gmail.com>
2023-07-25 11:13:46 +0530

    create dashboard structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './layout/header/header.component';
 import { FooterComponent } from './layout/footer/footer.component';
+import { DashboradComponent } from './dashborad/dashborad.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    DashboradComponent
   ],
   imports: [
     BrowserModule,
