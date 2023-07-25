57a42c8b7284186689611b4d97e76a6d5bb9d6ad
HimashaNethmini <himashanethmini05@gmail.com>
2023-07-25 15:58:31 +0530

    added the paths

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboradComponent } from './dashborad/dashborad.component';
+import { CategoriesComponent } from './categories/categories.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  //define the root route
+  { path: '', component: DashboradComponent },
+  { path: 'categories', component:CategoriesComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
