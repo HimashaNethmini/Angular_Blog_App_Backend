1a4d00584b525379f285ac03609a543af8aab13a
HimashaNethmini <himashanethmini05@gmail.com>
2023-07-25 15:33:18 +0530

    create firebase configurations

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,12 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://api.example.com/prod',
+    
+    firebaseConfig: {
+      apiKey: "AIzaSyD--TTb1yI-7xy0rtY_gnDLyjtT4Uixr2Y",
+      authDomain: "angular-backend-3ca34.firebaseapp.com",
+      projectId: "angular-backend-3ca34",
+      storageBucket: "angular-backend-3ca34.appspot.com",
+      messagingSenderId: "220158212831",
+      appId: "1:220158212831:web:205bc3de45d20aa3e3876a"
+  }
   };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,13 +1,5 @@
 export const environment = {
-    production: true,
-    // Your web app's Firebase configuration
-    firebaseConfig: {
-        apiKey: "AIzaSyD--TTb1yI-7xy0rtY_gnDLyjtT4Uixr2Y",
-        authDomain: "angular-backend-3ca34.firebaseapp.com",
-        projectId: "angular-backend-3ca34",
-        storageBucket: "angular-backend-3ca34.appspot.com",
-        messagingSenderId: "220158212831",
-        appId: "1:220158212831:web:205bc3de45d20aa3e3876a"
-    }
+    production: false,
+    
     
   };
