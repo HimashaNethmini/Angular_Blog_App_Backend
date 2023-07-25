eec60a9bcde83fdc8fc74ef29afce4e97a9bc983
HimashaNethmini <himashanethmini05@gmail.com>
2023-07-25 15:48:26 +0530

    importing firestore features

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AngularFireModule } from '@angular/fire/compat'; // Import AngularFireModule
-//import { AngularFireStoreModule } from '@angular/fire/firestore';
-
+import { FirestoreModule } from '@angular/fire/firestore';
 import  { environment } from 'src/environments/environment.prod';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -23,7 +22,8 @@ import { DashboradComponent } from './dashborad/dashborad.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    AngularFireModule.initializeApp(environment.firebaseConfig)
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    FirestoreModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
